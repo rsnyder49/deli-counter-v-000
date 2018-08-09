639fb2161430c2ed202fa5b4e7de90829e684f17
Robert Snyder <r.snyder49@gmail.com>
2018-08-09 19:07:19 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,9 @@ def line(array)
   if array.length == 0  
     puts "The line is currently empty."
   else 
-    array.collect{|name| puts "The line is currently name}
+    count = 1
+    while count < array.length 
+      puts "The line is currently #{count}. #{name}"
   end
 end 
 
