d43f9cccc5dceec0154226c061641417c1ca219b
Robert Snyder <r.snyder49@gmail.com>
2018-08-09 19:02:07 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,6 +5,13 @@ katz_deli = []
 def line(array)
   if array.length == 0  
     puts "The line is currently empty."
+  else 
+    array.each{|name| puts name}
   end
 end 
 
+def take_a_number(array, name)
+end 
+
+def now_serving(array) 
+end 
