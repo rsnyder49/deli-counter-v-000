3741ea8093fdc6a73ba3e41145485b6eed35bb02
Robert Snyder <r.snyder49@gmail.com>
2018-08-09 19:53:21 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -25,8 +25,8 @@ def line(array)
 end 
 
 def take_a_number(array, name)
-  new_line = array.push(name)
-  line(new_line)
+  array.push(name)
+  puts "Welcome, #{name}. You are number #{array.length} in line."
 end 
 
 def now_serving(array) 
